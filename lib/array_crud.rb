2f0882b800e4e6ddd7eb700a4961b115a4ee3dd8
jessi ortega <ortegabx05@gmail.com>
2019-07-03 17:04:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,8 +2,7 @@ def create_an_empty_array
   []
 end
 
-def create_an_array =
-  [one, two, three, four]
+def create_an_array =[one, two, three, four]
 end
 
 def add_element_to_end_of_array(array, element)
